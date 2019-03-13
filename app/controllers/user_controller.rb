9fd96d1d7af551856b7cd7d94eef590ff8c7653e
sato akihide <aki@sato.local>
2019-03-13 15:30:17 +0900

    userルーティング設定,gemfile修正

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,18 @@
 class UserController < ApplicationController
   def new
+    @user = User.new()
   end
+
+  def create
+    @user = User.new(set_params)
+    if @user.save
+      redirect_to 'top'
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+  def 
 end
