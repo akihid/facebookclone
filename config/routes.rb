ec916d201b9886b51a7f0a1233c1af70bd3d914a
sato akihide <aki@sato.local>
2019-03-14 16:31:40 +0900

    エラー404、500表示、メッセージのの日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  resources :posts do
+  resources :posts , only:[:index , :new ,:create , :edit , :update , :destroy]do
     collection do
       post :confirm
     end
   end
-  resources :sessions
+  resources :sessions , only:[:new ,:create , :destroy]
   resources :favorites , only:[:create , :destroy]
-  resources :users ,only:[:new,:create,:destroy] do
+  resources :users ,only:[:new , :create , :destroy] do
     collection do
       post :confirm
     end
-  end
 end
