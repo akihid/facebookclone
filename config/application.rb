ddeb778cc2df41172627ad47a0f76bec9438d459
sato akihide <aki@sato.local>
2019-03-16 01:41:04 +0900

    config修正

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,6 @@ module Facebookapp
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.assets.initialize_on_precompile = false
   end
 end
